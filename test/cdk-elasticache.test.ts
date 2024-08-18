7494db36063f645571c5469d0b875b742fc72766
seitamuro <seitamuro0210@gmail.com>
2024-08-18 21:04:05 +0900

    Initial commit

---
+++ test/cdk-elasticache.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkElasticache from '../lib/cdk-elasticache-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-elasticache-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkElasticache.CdkElasticacheStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
