2ae564ef26e6497c015e45e5a32b03bf02bc808e
seitamuro <seitamuro0210@gmail.com>
2024-08-18 21:39:28 +0900

    elasticacheを作成

--- lib/cdk-elasticache-stack.ts
+++ lib/cdk-elasticache-stack.ts
@@ -1,16 +1,43 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as elasticache from "aws-cdk-lib/aws-elasticache";
+import { Construct } from "constructs";
 
 export class CdkElasticacheStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, "VPC", {
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: "Public",
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 24,
+          name: "Private",
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+        },
+      ],
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkElasticacheQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const subnetGroup = new elasticache.CfnSubnetGroup(this, "SubnetGroup", {
+      cacheSubnetGroupName: "subnet-group",
+      subnetIds: vpc.privateSubnets.map((subnet) => subnet.subnetId),
+      description: "Subnet group for ElastiCache",
+    });
+
+    const redis = new elasticache.CfnReplicationGroup(this, "Redis", {
+      engine: "redis",
+      cacheNodeType: "cache.t3.micro",
+      engineVersion: "6.2",
+      replicasPerNodeGroup: 1,
+      numNodeGroups: 1,
+      replicationGroupDescription: "Redis Replication Group",
+      cacheSubnetGroupName: subnetGroup.cacheSubnetGroupName,
+      multiAzEnabled: true,
+    });
+    redis.addDependency(subnetGroup);
   }
 }
